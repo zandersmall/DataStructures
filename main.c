802f036cdc8b5b4c9990cc4338596666cd530212
Alexander Small <>
2023-01-17 17:33:27 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+   int a;
+   int *aPtr;
+
+   a = 7;
+   aPtr = &a;
+
+   printf("The address of a is %p"
+          "\nThe value of aPtr is %p", &a, aPtr );
+   printf( "\n\n The value of a is %d"
+          "\nThe value of *aPtr is %d", a, *aPtr );
+    printf( "\n\nShowing that * and & are complements of "
+           "each other \n&*aPtr = &p"
+           "\n*&aPtr = %p\n", &*aPtr, *&aPtr );
+           
+    return 0;
+}
